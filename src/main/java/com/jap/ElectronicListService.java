d1637def706365f4ba1a640529649b5afadd006f
Neeraj7979 <Imnsinha77@outlook.com>
2022-09-05 23:21:12 +0530

    Succesfully run all the test cases.

--- src/main/java/com/jap/ElectronicListService.java
+++ src/main/java/com/jap/ElectronicListService.java
@@ -7,19 +7,29 @@ import java.util.List;
 public class ElectronicListService {
     //add all the electronics items inside the List
     public static List<String> addElectronicsItemsToList(String electronicsItems) {
-       List<String> itemList = null;
+       List<String> itemList = new ArrayList<>();
+       itemList.add("computer");
+       itemList.add("Refrigerator");
+       itemList.add("smartphone");
+       itemList.add("printer");
+
         return itemList;
     }
 
     //search the electronic item from the list and return the index position
     public static int searchElectronicItemInList(List<String> itemList, String searchItem) {
-        return -1;
+        int index=itemList.indexOf("Refrigerator");
+        return index;
+
     }
 
     //remove the electronic item from the list using iterator
     public static boolean removeElectronicsItemFromList(List<String> itemList, String removeItem)
     {
         boolean flag = false;
+        for (String e:itemList){
+            itemList.remove(removeItem);
+        }
 
         return flag;
     }
